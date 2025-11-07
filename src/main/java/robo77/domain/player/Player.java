7fdb136a6969a3d72e4d74d51828ca9637bb7145
Jiihyun <cwlgusc@gmail.com>
2025-11-07 22:26:56 +0900

    feat: 게임 참여자 등록 기능 구현
    - Player 도메인 생성

---
+++ src/main/java/robo77/domain/player/Player.java
@@ -0,0 +1,15 @@
+package robo77.domain.player;
+
+import robo77.domain.Hand;
+
+public class Player {
+
+    private final Name name;
+
+    private final Hand hand;
+
+    public Player(String name, Hand hand) {
+        this.name = new Name(name);
+        this.hand = hand;
+    }
+}
