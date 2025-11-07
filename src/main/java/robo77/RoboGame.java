03c97e634c8bebb5d0e35633afa1e58a419fe2fe
Jiihyun <cwlgusc@gmail.com>
2025-11-08 02:08:10 +0900

    feat: 참여자 카드 제출 흐름 구현

--- src/main/java/robo77/RoboGame.java
+++ src/main/java/robo77/RoboGame.java
@@ -1,9 +1,11 @@
 package robo77;
 
 import java.util.List;
+import robo77.domain.Card;
 import robo77.domain.Deck;
 import robo77.domain.Hand;
 import robo77.domain.player.Player;
+import robo77.exception.ExceptionMessage;
 import robo77.view.InputView;
 import robo77.view.OutputView;
 
@@ -24,5 +26,13 @@ public class RoboGame {
         Player player = new Player(playerName, hands.getFirst());
         Player bot = new Player(playerName, hands.getLast());
         outputView.showSumAndHandMessage(0, player.getHand());
+        String cardToSubmit = inputView.readCardToSubmit();
+        Card submittedCard = Card.from(cardToSubmit);
+        boolean hasCard = player.hasSubmittedCard(submittedCard);
+        if (!hasCard) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_CARD.getMessage());
+        }
+        Card newCard = deck.shareCard();
+        player.submitCard(submittedCard, newCard);
     }
 }
