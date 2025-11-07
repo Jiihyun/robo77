c661da89244bf92c02498e4426c136e546014566
Jiihyun <cwlgusc@gmail.com>
2025-11-07 19:53:13 +0900

    feat: 플레이어 이름 VO 생성
    - 이름 길이 검증 추가
    - 이름 형식 검증 추가
    - 이름 테스트 추가

---
+++ src/main/java/robo77/domain/player/Name.java
@@ -0,0 +1,48 @@
+package robo77.domain.player;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import robo77.exception.ExceptionMessage;
+
+public class Name {
+
+    private static final int MIN_LENGTH = 2;
+    private static final int MAX_LENGTH = 10;
+    private static final Pattern FORMAT_PATTERN = Pattern.compile("^[0-9a-z]+$");
+
+    private final String value;
+
+    public Name(String value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(String value) {
+        validateLength(value);
+        validateFormat(value);
+    }
+
+    private void validateLength(String value) {
+        if (value.isBlank()) {
+            throw new IllegalArgumentException(ExceptionMessage.EMPTY_PLAYER_NAME.getMessage());
+        }
+        if (isOutOfRange(value.length())) {
+            throw new IllegalArgumentException(ExceptionMessage.PLAYER_NAME_OUT_OF_RANGE.getMessage());
+        }
+    }
+
+    private boolean isOutOfRange(int length) {
+        return length < MIN_LENGTH || length > MAX_LENGTH;
+    }
+
+    private void validateFormat(String value) {
+        Matcher matcher = FORMAT_PATTERN.matcher(value);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(ExceptionMessage.WRONG_PLAYER_NAME_FORMAT.getMessage());
+        }
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
--- src/main/java/robo77/exception/ExceptionMessage.java
+++ src/main/java/robo77/exception/ExceptionMessage.java
@@ -4,6 +4,9 @@ package robo77.exception;
 public enum ExceptionMessage {
 
     BLANK_INPUT("값을 입력하지 않으셨습니다."),
+    EMPTY_PLAYER_NAME("사용자 이름은 빈문자열일 수 없습니다."),
+    PLAYER_NAME_OUT_OF_RANGE("입력 가능한 이름 길이 범위를 초과하였습니다."),
+    WRONG_PLAYER_NAME_FORMAT("잘못된 사용자 이름 형식입니다."),
     ;
 
     private final String message;
