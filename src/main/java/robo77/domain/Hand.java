1012c8c2b68458bdb3b5704cbc170dbb6115fdad
Jiihyun <cwlgusc@gmail.com>
2025-11-07 22:05:54 +0900

    feat: 손패 카드 개수 범위 검증 추가
    - Hand 도메인 추가

---
+++ src/main/java/robo77/domain/Hand.java
@@ -0,0 +1,22 @@
+package robo77.domain;
+
+import java.util.List;
+import robo77.exception.ExceptionMessage;
+
+public class Hand {
+
+    private static final int HOLDING_CARD_SIZE = 5;
+
+    private final List<Card> holdingCards;
+
+    public Hand(List<Card> holdingCards) {
+        validateCardSize(holdingCards);
+        this.holdingCards = holdingCards;
+    }
+
+    private void validateCardSize(List<Card> holdingCards) {
+        if (holdingCards.size() != HOLDING_CARD_SIZE) {
+            throw new IllegalArgumentException(ExceptionMessage.HOLDING_CARD_OUT_OF_RANGE.getMessage());
+        }
+    }
+}
--- src/main/java/robo77/exception/ExceptionMessage.java
+++ src/main/java/robo77/exception/ExceptionMessage.java
@@ -7,6 +7,7 @@ public enum ExceptionMessage {
     EMPTY_PLAYER_NAME("사용자 이름은 빈문자열일 수 없습니다."),
     PLAYER_NAME_OUT_OF_RANGE("입력 가능한 이름 길이 범위를 초과하였습니다."),
     WRONG_PLAYER_NAME_FORMAT("잘못된 사용자 이름 형식입니다."),
+    HOLDING_CARD_OUT_OF_RANGE("손패 범위를 초과하였습니다."),
     ;
 
     private final String message;
