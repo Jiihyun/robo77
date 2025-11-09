d2f615e1adfde4b9a3eb0165a3f9701f1bdf4a21
Jiihyun <cwlgusc@gmail.com>
2025-11-10 02:27:35 +0900

    fix: 손패에 카드가 있는지 확인 여부 로직 수정

--- src/main/java/robo77/domain/Hand.java
+++ src/main/java/robo77/domain/Hand.java
@@ -22,7 +22,7 @@ public class Hand {
 
     public boolean hasCard(Card submittedCard) {
         return holdingCards.stream()
-                .noneMatch(card -> card.equals(submittedCard));
+                .anyMatch(card -> card.equals(submittedCard));
     }
 
     public void removeCard(Card submittedCard) {
